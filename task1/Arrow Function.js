94c6363e81d6cbcbc84b9ff654ea1c44c53c9070
Mikhailo479 <misalelek843@gmail.com>
2024-11-08 15:04:09 +0200

    Add files via upload

---
+++ task1/Arrow Function.js
@@ -0,0 +1,4 @@
+const calculateAreaArrow = (width, height) => width * height;
+
+const area2 = calculateAreaArrow(5, 10);
+console.log(area2); // Виведе: 50
---
+++ task1/Function Expression.js
@@ -0,0 +1,6 @@
+const calculateArea = function (width, height) {
+  return width * height;
+};
+
+const area1 = calculateArea(5, 10);
+console.log(area1); // Виведе: 50
