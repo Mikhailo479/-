01cc9e41a7475361130b26c3e35c809e90e5f572
Mikhailo479 <misalelek843@gmail.com>
2024-11-12 09:46:26 +0200

    Delete task1 directory

--- task1/Arrow Function.js
+++
@@ -1,4 +0,0 @@
-const calculateAreaArrow = (width, height) => width * height;
-
-const area2 = calculateAreaArrow(5, 10);
-console.log(area2); // Виведе: 50
--- task1/Function Expression.js
+++
@@ -1,6 +0,0 @@
-const calculateArea = function (width, height) {
-  return width * height;
-};
-
-const area1 = calculateArea(5, 10);
-console.log(area1); // Виведе: 50
